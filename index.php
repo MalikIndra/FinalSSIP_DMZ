0d88256a8f626d7c79be5f8948aaf7b8ea7c524b
zlaia06 <58580953+zlaia06@users.noreply.github.com>
2019-12-13 10:10:32 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,97 +1,133 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<title>APLIKASI PERPUSTAKAAN</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-* {
-  box-sizing: border-box;
-}
+    <head>
+      <title>APLIKASI PERPUSTAKAAN</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <script type="text/javascript" src="../js/jquery-3.4.1.js"></script>
+      <script type="text/javascript" src="../js/bootstrap.js"></script>
+      <link href="../css/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+      <link href="../css/sb-admin.css" rel="stylesheet">
+    </head>
+    <body>
+      <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+              <a class="navbar-brand mr-1" href="index.php">Perpustakaan</a>
+              <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+              <i class="fas fa-bars"></i>
+              </button>
+              <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+              <div class="input-group">
+                  <input type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                  <div class="input-group-append">
+                  <button class="btn btn-primary" type="button">
+                      <i class="fas fa-search"></i>
+                  </button>
+                  </div>
+              </div>
+              </form>
+          </nav>
 
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
+          <div id="wrapper">
+              <!-- Sidebar -->
+              <ul class="sidebar navbar-nav">
+                  <li class="nav-item active">
+                      <a class="nav-link" href="index.php">
+                          <i class="fas fa-fw fa-tachometer-alt"></i>
+                          <span>Menu</span>
+                      </a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="DataSiswa.php">
+                          <i class="fas fa-fw fa-folder"></i>
+                          <span>Data Siswa</span>
+                      </a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="DataPeminjaman.php">
+                          <i class="fas fa-fw fa-chart-area"></i>
+                          <span>Data Peminjaman</span></a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="#">
+                          <i class="fas fa-fw fa-table"></i>
+                          <span>Data Buku</span></a>
+                  </li>
+              </ul>
 
-/* Style the header */
-header {
-  background-color: red;
-  padding: 30px;
-  text-align: center;
-  font-size: 35px;
-  color: white;
-}
+              <div id="content-wrapper">
+                  <div class="container-fluid">
+                      <ol class="breadcrumb">
+                          <li class="breadcrumb-item">
+                              <a href="index.php">Menu</a>
+                          </li>
+                          <li class="breadcrumb-item active">Halaman Utama</li>
+                      </ol>
+                      <h1 class="display-1">SELAMAT DATANG</h1>
+                      <hr>
+                        <div class="container ml-1">Buku adalah Jembatan Ilmu <br><br></div>  
+                        
+                        <div class="row ml-1">
+                          <div class="col-xl-3 col-sm-6 mb-3">
+                            <div class="card text-white bg-primary o-hidden h-100">
+                              <div class="card-body">
+                                <div class="card-body-icon">
+                                  <i class="fas fa-fw fa-list"></i>
+                                </div>
+                                <div class="mr-5">Data Siswa</div>
+                              </div>
+                              <a class="card-footer text-white clearfix small z-1" href="DataSiswa.php">
+                                <span class="float-left">View Details</span>
+                                <span class="float-right">
+                                  <i class="fas fa-angle-right"></i>
+                                </span>
+                              </a>
+                            </div>
+                          </div>
 
-/* Create two columns/boxes that floats next to each other */
-nav {
-  float: left;
-  width: 30%;
-  height: 300px; /* only for demonstration, should be removed */
-  background: yellow;
-  padding: 20px;
-}
+                          <div class="col-xl-3 col-sm-6 mb-3">
+                            <div class="card text-white bg-warning o-hidden h-100">
+                              <div class="card-body">
+                                <div class="card-body-icon">
+                                  <i class="fas fa-fw fa-list"></i>
+                                </div>
+                                <div class="mr-5">Data Pinjaman</div>
+                              </div>
+                              <a class="card-footer text-white clearfix small z-1" href="DataPeminjaman.php">
+                                <span class="float-left">View Details</span>
+                                <span class="float-right">
+                                  <i class="fas fa-angle-right"></i>
+                                </span>
+                              </a>
+                            </div>
+                          </div>
 
-/* Style the list inside the menu */
-nav ul {
-  list-style-type: none;
-  padding: 0;
-}
+                          <div class="col-xl-3 col-sm-6 mb-3">
+                            <div class="card text-white bg-danger o-hidden h-100">
+                              <div class="card-body">
+                                <div class="card-body-icon">
+                                  <i class="fas fa-fw fa-list"></i>
+                                </div>
+                                <div class="mr-5">Data Buku</div>
+                              </div>
+                              <a class="card-footer text-white clearfix small z-1" href="#">
+                                <span class="float-left">View Details</span>
+                                <span class="float-right">
+                                  <i class="fas fa-angle-right"></i>
+                                </span>
+                              </a>
+                            </div>
+                          </div>
+                          </div>
+                        </div>
 
-article {
-  float: left;
-  padding: 20px;
-  width: 70%;
-  background-color: lightblue;
-  height: 300px; /* only for demonstration, should be removed */
-}
-
-/* Clear floats after the columns */
-section:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-/* Style the footer */
-footer {
-  background-color: #777;
-  padding: 10px;
-  text-align: center;
-  color: white;
-}
-
-/* Responsive layout - makes the two columns/boxes stack on top of each other instead of next to each other, on small screens */
-@media (max-width: 600px) {
-  nav, article {
-    width: 100%;
-    height: auto;
-  }
-}
-</style>
-</head>
-<body>
-<header>
-  <h2>APLIKASI PERPUSTAKAAN</h2>
-</header>
-
-<section>
-  <nav>
-    <ul>
-      <li><a href="DataSiswa.php">Data Siswa</a></li>
-      <li><a href="DataPeminjaman.php">Data Peminjaman</a></li>
-      <li><a href="DataBuku.php">Data Buku</a></li>
-    </ul>
-  </nav>
-  
-  <article>
-    <h1>SELAMAT DATANG</h1>
-    <p>Buku adalah jembatan ilmu</p>
-  </article>
-</section>
-
-<footer>
-  <p>Created By Dhika Pangestu Irawan , Malik Indrayanto, Zefanya Ohito Dodo Abram Laia</p>
-</footer>
-
+                      <footer class="sticky-footer">
+                          <div class="container my-auto">
+                              <div class="copyright text-center my-auto">
+                              <span>Created by: Dhika Pangestu Irawan, Malik Indrayanto, Zefanya Ohito Dodo Abram Laia</span>
+                              </div>
+                          </div>
+                      </footer>
+                  </div>
+              </div>
 </body>
 </html>
