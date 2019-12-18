ce169b4343c6f530e59199e60556c7ab14472479
dhikapange13 <58808715+dhikapange13@users.noreply.github.com>
2019-12-18 08:40:08 +0700

    Delete HapusBuku.php

--- HapusBuku.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include 'koneksi.php';
-    if (isset($_GET['ID_Buku'])){
-        $delete = mysqli_query($conn, "DELETE FROM buku WHERE ID_Buku = '".$_GET['ID_Buku']."'");
-        header('location:DataBuku.php');
-    }
-?>
